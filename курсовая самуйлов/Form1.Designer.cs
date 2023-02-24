a430428af1e7a762cc7f4f551f95c5fd3d91fc16
Bobkov Maxim <bobkov.maxim2018@yandex.ru>
2023-02-24 11:27:25 +0300

    first commit

---
+++ курсовая самуйлов/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿
+namespace курсовая_самуйлов
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите размерность";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(195, 21);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(132, 22);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(74, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Отсортировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(427, 12);
+            this.chart1.Name = "chart1";
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series13.Legend = "Legend1";
+            series13.Name = "прямое включение";
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series14.Legend = "Legend1";
+            series14.Name = "двоичное включение";
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series15.Legend = "Legend1";
+            series15.Name = "сортировка Шелла";
+            this.chart1.Series.Add(series13);
+            this.chart1.Series.Add(series14);
+            this.chart1.Series.Add(series15);
+            this.chart1.Size = new System.Drawing.Size(805, 330);
+            this.chart1.TabIndex = 20;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(427, 368);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(805, 189);
+            this.dataGridView1.TabIndex = 21;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Длина односвязного списка";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Время сортировки прямым включением";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Время сортировки двоичным включением";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Время сортировки методом Шелла";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1157, 646);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Выйти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Location = new System.Drawing.Point(356, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(15, 687);
+            this.panel1.TabIndex = 23;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 85);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 206);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите тип упорядоченности";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(14, 157);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 14;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Процент упорядоченности";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(14, 109);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(224, 21);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.Text = "Частично упорядоченный";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(14, 82);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(306, 21);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.Text = "Упорядоченный в обратном порядке";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(14, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(149, 21);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "Упорядоченный";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(14, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(167, 21);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.Text = "Неупорядоченный";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "(от 1000 до 100000)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 717);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Курсовая работа Бобков Максим 21вп2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ курсовая самуйлов/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace курсовая_самуйлов
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        int size;
+        List<int> list_basic = new List<int>(); //односвязный список базовый
+        List<int> list = new List<int>(); //односвязный список для сортировок
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //размерность списка
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            //процент упорядоченности
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //сортировака прямым включение
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //сортировака двоичным включение
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //сортировака методом шелла
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //кнопка отсортировать
+            size = (int)numericUpDown1.Value;
+            if (size == 0)
+            {
+                MessageBox.Show("Вы не ввели размерность односвязного списка!");
+                Application.Restart();
+            }
+            else if (!radioButton1.Checked && !radioButton2.Checked && !radioButton3.Checked && !radioButton4.Checked)
+            {
+                MessageBox.Show("Выберите один из четырёх типов упорядоченности!");
+                Application.Restart();
+            }
+
+
+            ClearGraphic();
+            CleatTable();
+            int count_nax = 10;//кол-во шагов
+            int right = size / 10;//правая граница
+            int nax = size / 10;//промежуток между шагами
+            int TimeBasic = 0;
+            int TimeDouble = 0;
+            int TimeShell = 0;
+            
+
+            for (int k = 0; k < count_nax; k++)
+            {
+                update_list(list_basic, list,right);
+                TimeBasic += inclusionSort(list, right);// прямым включением
+
+                update_list(list_basic, list, right);
+                TimeDouble += doubleinclusionSort(list, right);//двоичным включением
+
+                update_list(list_basic, list, right);
+                TimeShell += ShellSort(list, right);//сортировка шелла
+                
+                Graphic(TimeBasic, TimeDouble, TimeShell, right);
+                Table(TimeBasic, TimeDouble, TimeShell, right);
+
+                right = nax * (k + 2);
+            }
+        }
+
+        private void update_list(List<int> list_basic, List<int> list,  int right)
+        {
+            list.Clear();
+            for (int i = 0; i < right; i++)
+            {
+                list.Add(list_basic.ElementAt(i));
+            }
+           
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //кнопка выйти
+            Close();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //неупорядоченный
+            size = (int)numericUpDown1.Value;
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                list_basic.Add(rnd.Next(0, size));
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //упорядоченный
+            size = (int)numericUpDown1.Value;
+            Random rnd = new Random();
+            list_basic.Add(rnd.Next(0, size));
+            for (int i = 1; i < size; i++)
+            {
+                list_basic.Add(list_basic.ElementAt(i - 1) + rnd.Next(0, size));
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            //упорядоченный в обратном порядке
+            size = (int)numericUpDown1.Value;
+            Random rnd = new Random();
+            list_basic.Add(rnd.Next(0, size));
+            for (int i = 1; i < size; i++)
+            {
+                list_basic.Add(list_basic.ElementAt(i - 1) + rnd.Next(0, size));
+            }
+            list_basic.Reverse();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            //частично упорядоченный
+            size = (int)numericUpDown1.Value;
+            int percent = (int)numericUpDown2.Value;
+            int count = (size * percent) / 100;
+            Random rnd = new Random();
+            list_basic.Add(rnd.Next(0, size));
+            for (int i = 1; i < count; i++)
+            {
+                list_basic.Add(list_basic.ElementAt(i - 1) + rnd.Next(0, size));
+            }
+            for (int i = count; i < size; i++)
+            {
+                list_basic.Add(rnd.Next(0, size));
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ClearGraphic()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                this.chart1.Series[i].Points.Clear();
+            }
+            this.chart1.Update();
+        }
+
+        private void CleatTable()
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void Graphic(int first_sort, int second_sort, int third_sort, int number)
+        {
+            this.chart1.Series[0].Points.AddXY(number, first_sort);
+            this.chart1.Series[1].Points.AddXY(number, second_sort);
+            this.chart1.Series[2].Points.AddXY(number, third_sort);
+        }
+
+        private void Table(int first_sort, int second_sort, int third_sort, int number)
+        {
+            dataGridView1.Rows.Add(number, first_sort, second_sort, third_sort);
+        }
+
+        int inclusionSort(List<int> list, int size)
+        {
+            int StartTime = Environment.TickCount;
+            for (int i = 1; i < size; i++)
+            {
+                int value = list[i]; 
+                int index = i;     
+                while ((index > 0) && (list[index - 1] > value))
+                {   
+                    list[index] = list[index - 1];
+                    index--;    
+                }
+                list[index] = value; 
+            }
+            return Environment.TickCount - StartTime;
+        }
+
+        int ShellSort(List<int> list, int size)
+        {
+            int StartTime = Environment.TickCount;
+            int i, j, step;
+            int tmp;
+            for (step = size / 2; step > 0; step /= 2)
+                for (i = step; i < size; i++)
+                {
+                    tmp = list[i];
+                    for (j = i; j >= step; j -= step)
+                    {
+                        if (tmp < list[j - step])
+                            list[j] = list[j - step];
+                        else
+                            break;
+                    }
+                    list[j] = tmp;
+                }
+            return Environment.TickCount - StartTime;
+        }
+
+        int doubleinclusionSort(List<int> list, int size)
+        {
+            int StartTime = Environment.TickCount;
+            int j, m, left, right, x;
+            for (int i = 1; i < size; i++)
+                if (list[i - 1] > list[i])
+                {
+                    x = list[i];
+                    left = 0;
+                    right = i - 1;
+                    do
+                    {
+                        m = (left + right) / 2;
+                        if (list[m] < x) left = m + 1;
+                        else right = m - 1;
+                    } while (left <= right);
+                    for (j = i - 1; j >= left; j--)
+                        list[j + 1] = list[j];
+                    list[left] = x;
+                }
+            return Environment.TickCount - StartTime;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
